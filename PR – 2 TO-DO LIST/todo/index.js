27f5306005ea307b9f78fd2c17f6f613d7b38e94
yash <officialyashvora1978@gmail.com>
2023-09-21 11:12:24 +0530

    test

--- PR – 2 TO-DO LIST/todo/index.js
+++ PR – 2 TO-DO LIST/todo/index.js
@@ -17,18 +17,39 @@ app.post("/addtodo",(req,res)=>{
 });
 app.patch("/update/:id",(req,res)=>{
     let {id} = req.params
-    let updatedTodo = todos.filter(todo=>todo.id == id)
+    console.log(id);
+    let updatedTodo = initialTodo.filter(todo=>todo.id == id)
+    console.log(updatedTodo);
     updatedTodo[0].title=req.body.title
     updatedTodo[0].isCompleted=req.body.isCompleted
     res.status(200).send(...updatedTodo)
 })
 app.delete("/delete/:id",(req,res)=>{
     let {id} = req.params
-    let index = todos.findIndex(todo=>todo.id === id)
-    let deletedtodo = todos.splice(index, 1)[0]
-    res.status(200).send({deletedtodo,todos})
+    let index = initialTodo.findIndex(todo=>todo.id == id)
+    let deletedtodo = initialTodo.splice(index,1)[0]
+    res.status(200).send({deletedtodo, initialTodo})
 })
-
+app.get("/todo/:id",(req,res)=>{
+    let {id}= req.params
+    let todo = initialTodo.filter(todo=>todo.id==id)
+    console.log(todo);
+    res.status(200).send(todo)
+})
+app.get("/findbystatus",(req,res)=>{
+    let isCompleted = req.query.isCompleted
+    if(isCompleted === "true"){
+        completedtodos = initialTodo.filter(todo=>todo.isCompleted===true)
+        res.send(completedtodos)
+    }
+    else if(isCompleted === "false"){
+        let notCompletedtodos = initialTodo.filter(todo=>todo.isCompleted === false)
+        res.send(notCompletedtodos)
+    }
+    else{
+        res.send("Invalid")
+    }
+})
 
  app.listen(8090,()=>{
     console.log("Server Started");
